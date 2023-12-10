9d2e19852dc3ced3e6fc6677b78ce6c1b451f444
Aly <alye067@gmail.com>
2023-12-10 06:07:14 +0200

    New Task 2

--- lib/11111.dart
+++ lib/11111.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:projects/InstaPage.dart';
 import 'package:projects/Login.dart';
 import 'package:projects/Screen%202%20Task%203.dart';
 import 'package:projects/Screen%203%20task%202.dart';
@@ -14,7 +15,7 @@ import 'package:projects/screen%201%20task%202.dart';
 void main(){
   runApp(
     MaterialApp(
-      home: SignUp(),
+      home: InstaPage(),
     ),
   );
 }
---
+++ lib/InstaPage.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+
+abstract class User{
+  String? username;
+  String? image;
+
+  User({this.username,this.image});
+}
+
+class Posts extends User{
+  String? postImage;
+
+  Posts({super.image,super.username,this.postImage});
+}
+
+
+
+class InstaPage extends StatelessWidget {
+  const InstaPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        leadingWidth: 120,
+        leading: Row(
+          children: [
+            Text('Instagram',style: TextStyle(color: Colors.white,fontSize: 20),),
+            Icon(Icons.keyboard_arrow_down,color: Colors.white,)
+          ],
+        ),
+        actions: [
+          Icon(MdiIcons.plusBoxOutline,color: Colors.white,),
+          SizedBox(width: 20,),
+          Icon(MdiIcons.heartOutline,color: Colors.white,),
+          SizedBox(width: 20,),
+          Stack(
+            alignment: Alignment.topRight,
+              children: [
+                Container(
+                  height: 30,
+                    child: Image.asset('images/image9.png', color: Colors.white,height: 20,width: 20,)),
+                Container(
+                  height: 10,
+                  width: 10,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    shape: BoxShape.circle
+                  ),
+                    alignment: Alignment.center,
+                    child: PositionedDirectional(
+                      end: 20,
+                        child: Text('6',style: TextStyle(color: Colors.white  ,fontSize: 8),)))
+              ]
+          ),
+          SizedBox(width: 20,),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        showSelectedLabels: true,
+        fixedColor: Colors.white,
+        unselectedItemColor: Colors.white,
+        backgroundColor: Colors.black,
+        showUnselectedLabels: !true,
+        items: [
+          BottomNavigationBarItem(
+              icon:Icon(Icons.home_filled),
+            label: 'Home'
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'search'
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.ondemand_video_sharp),
+              label: 'videos'
+          ),
+          BottomNavigationBarItem(
+              icon:Icon(Icons.shopping_bag_outlined),
+              label: 'shop'
+          ),
+          BottomNavigationBarItem(
+              icon:CircleAvatar(
+                backgroundColor: Colors.white,
+                radius: 20,
+                backgroundImage: Image.asset('images/image12.png',color: Colors.white, height: 30, width: 30,fit: BoxFit.contain,).image,
+              ),
+              label: 'profile'
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  CircleAvatar(
+                    backgroundColor: Colors.white,
+                      radius: 20,
+                    backgroundImage: Image.asset('images/image12.png',color: Colors.white, height: 30, width: 30,fit: BoxFit.contain,).image,
+                      ),
+                  SizedBox(width: 20,),
+                  Text('dude.coder',style: TextStyle(fontSize: 20,color: Colors.white),),
+                  SizedBox(width: 190,),
+                  Icon(MdiIcons.dotsHorizontal,color: Colors.white,),
+                ],
+              ),
+              SizedBox(height: 5,),
+              Image.asset('images/image11.png'),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Icon(Icons.favorite_border,color: Colors.white,size: 30,),
+                  SizedBox(width: 10,),
+                  Icon(MdiIcons.chatOutline,color: Colors.white,size: 30),
+                  SizedBox(width: 10,),
+                  Icon(MdiIcons.sendVariantOutline,color: Colors.white,size: 30),
+                  Expanded(child: Image.asset('images/image14.png',height: 50,width: 50,color: Colors.grey,)),
+                  Icon(Icons.bookmark_outline,color: Colors.white,size: 30,),
+                ],
+              ),
+              SizedBox(height: 5,),
+              Row(
+                children: [
+                  CircleAvatar(
+                    radius: 17,
+                    backgroundImage: Image.asset('images/image13.png',fit: BoxFit.contain,).image,
+                  ),
+                  SizedBox(width: 10,),
+                  Text('Liked by flutter.deviser and 44 others',style: TextStyle(color: Colors.white, fontSize: 15),),
+                ],
+              ),
+              SizedBox(height: 5,),
+              Text('dude.coder Firebase In-App Messaging helps you engage you app\'s active by sending them targeted .... more',
+              style: TextStyle(color: Colors.white,fontSize: 15),),
+              SizedBox(height: 5,),
+              Text('View 1 comment',style: TextStyle(color: Colors.grey,fontSize: 17),)
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
